7313a8a87435f76bd1675fa71db94c7ca1a3d147
Luis <luis152288@gmail.com>
2017-09-13 11:53:24 -0400

    carousel

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -5,14 +5,16 @@
                 <div id="carouselHacked" class="carousel slide carousel-fade" data-ride="carousel">
                     <div class="header-backup"></div>
                     <div class="carousel-inner" role="listbox">
+                        @foreach($carousel as $carousel)
                         <div class="item active">
-                            <img src="img/slide-one.jpg" alt="">
+                            <img src="{{ asset('imagenIndex/'.$carousel->imagen) }}" alt="....">
                             <div class="carousel-caption">
-                                <h1>providing</h1>
-                                <p>highquality service for men &amp; women</p>
+                                <h1>{{ $carousel->titulo }}</h1>
+                                <p>{{ $carousel->subtitulo }}</p>
                             </div>
                         </div>
-                        <div class="item">
+                        @endforeach
+                        <!--<div class="item">
                             <img src="img/slide-two.jpg" alt="">
                             <div class="carousel-caption">
                                 <h1>providing</h1>
@@ -33,7 +35,7 @@
                                 <p>highquality service for men &amp; women</p>
                             </div>
                         </div>
-                    </div>
+                    </div>-->
                     <!-- Controls -->
                     <a class="left carousel-control" href="#carouselHacked" role="button" data-slide="prev">
                         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
